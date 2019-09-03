458c09ff2c1ec3b6ac0d85079a311c2647d69bb3
jainsuchi21 <jainsuchi21@gmail.com>
2019-09-03 16:52:19 +0530

    Add practice files

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <p>Send us a message!</p>
+  </Layout>
+)
